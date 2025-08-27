94ddd68ba14a5644790f57a2d9746d73b64f1fa2
youareeverysingleday <1992691699@qq.com>
2025-08-27 16:03:35 +0800

    use model in API on 20250827.

--- HttpService.py
+++ HttpService.py
@@ -12,7 +12,9 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.responses import FileResponse, JSONResponse
 import asyncio
-import requests
+import torch
+import torch.nn as nn
+# import requests
 
 # 提供接口。
 # uvicorn Service:app --reload
@@ -42,6 +44,7 @@ async def favicon():
 class Input(BaseModel):
     clientID: int # 使用者的身份ID。
     userID: int # 被预测对象的ID。
+    topK: int # 需要提供几个备选位置。
     length: int # 区域ID和时间戳的长度。
     regionID : List[int]
     timestamp : List[str]
@@ -89,14 +92,111 @@ def WriteLog(clientID, userID, nextLocation, length, historyTrajectories, log_pa
             writer.writerow(columnsName)
             writer.writerow(log_infor)
 
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        x = x + self.pe[:, : x.size(1)].requires_grad_(False)
+        return self.dropout(x)
+
+class CustomTransformer(nn.Module):
+    def __init__(self, num_geohash, d_model, nhead=4, num_layers=2, ff_dim=512, dropout=0.1, padding_idx=2):
+        super(CustomTransformer, self).__init__()
+        self.padding_idx = padding_idx
+        # print("initial d_model= {}.".format(d_model))
+        self.embedding = nn.Embedding(num_embeddings=num_geohash, embedding_dim=d_model)
+        self.dropout_emb = nn.Dropout(p=dropout)
+        self.transformer = nn.Transformer(d_model=d_model, 
+                                          num_encoder_layers=num_layers, 
+                                          num_decoder_layers=num_layers, 
+                                          dropout=dropout,
+                                          dim_feedforward=ff_dim, 
+                                          batch_first=True, 
+                                          nhead=nhead)
+
+        self.positional_encoding = PositionalEncoding(d_model, dropout=dropout)
+        self.MLPModel = torch.nn.Sequential(
+            nn.Linear(d_model, 128),
+            torch.nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(128, num_geohash)
+        )
+
+    def get_key_padding_mask(self, tokens):
+        """
+        返回 True 表示该位置为 padding（被 mask）
+        输入：tokens: [batch, seq_len]
+        输出：mask: [batch, seq_len]，bool
+        """
+        return (tokens == self.padding_idx)
+    
+    def forward(self, src, tgt):
+        # src_key_padding_mask = CustomTransformer.get_key_padding_mask(src)
+        # tgt_key_padding_mask = CustomTransformer.get_key_padding_mask(tgt)
+        src_key_padding_mask = self.get_key_padding_mask(src).to(device)
+        tgt_key_padding_mask = self.get_key_padding_mask(tgt).to(device)
+        tgt_mask = nn.Transformer.generate_square_subsequent_mask(tgt.size()[1]).to(device)
+        # tgt_mask = torch.triu(torch.ones(tgt.size()[1], tgt.size()[1]), diagonal=1).bool()
+        # src = self.embedding(src.long())
+        # tgt = self.embedding(tgt.long())
+        src = self.dropout_emb(self.embedding(src.long()))
+        tgt = self.dropout_emb(self.embedding(tgt.long()))
+        src = self.positional_encoding(src)
+        tgt = self.positional_encoding(tgt)
+        # print(tgt.shape)
+        # print(src.dtype, tgt.dtype)
+        out_t = self.transformer(src, tgt,
+                                 tgt_mask=tgt_mask,
+                                 src_key_padding_mask=src_key_padding_mask,
+                                 tgt_key_padding_mask=tgt_key_padding_mask,
+                                 memory_key_padding_mask=src_key_padding_mask  # 注意：memory 是 encoder 的输出
+                                )
+        
+        out = self.MLPModel(out_t)
+        return out_t, out
+
+gNum_geohash = 21000
+g_d_model = 28
 
 async def ExecuteModel(input: Input):
     print(input.userID)
     await asyncio.sleep(10) 
+    # 设置日志。
     time_day_stamp = time.strftime('%Y%m%d', time.localtime())
     log_save_path = f'./log/' + 'log_{}.csv'.format(time_day_stamp)
 
+    # 加载模型并且预测。
+    # ----------------start-----------------
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    # 获得历史轨迹。
+    HistoryGrid = torch.tensor(input.regionID)
+    # 获取topk的值。
+    topK= input.topK
+
+    # 加载模型。
+    predictModel = CustomTransformer(num_geohash=gNum_geohash, d_model=g_d_model)
+    checkpoint = torch.load('./model.pth')
+    predictModel.load_state_dict(checkpoint['model_state_dict'])
+
+    src = HistoryGrid.unsqueeze(0).to(device)
+    tgt = src[:, -1].unsqueeze(0).to(device)
+
+    _, out = predictModel(src, tgt)
+    _, output_topk_index = torch.topk(out[0, -1, :], k=topK, dim=-1)
+
     nextLocation = 925
+    # ----------------end-----------------
 
     WriteLog(clientID=input.clientID, userID=input.userID, nextLocation=nextLocation, length=input.length, 
              historyTrajectories=input.regionID, log_path=log_save_path)
--- README.md
+++ README.md
@@ -12,6 +12,7 @@ uvicorn HttpService:app --host 0.0.0.0 --port 8000 --reload
 {
     "clientID": 1,
     "userID": 2,
+    "topK": 3,
     "length": 100,
     "regionID": [1002, 2345, 5678, ......],
     "timestamp": ["2025-01-28 08:00:00", "2025-02-10 09:10:00", "2025-03-05 08:20:00", ......]
@@ -20,10 +21,11 @@ uvicorn HttpService:app --host 0.0.0.0 --port 8000 --reload
 
 1. clientID 是用户身份标识，可以为空。
 2. userID 是被预测对象的ID，是一个正整数值。不能为空。
-3. regionID 是一个以正整数（不包含0的自然数）为元素的链表。0 是作为填充值存在的。不能为空。
-4. length 表示的是 regionID 和 timestamp 所对应的链表的长度。默认情况下 length 的长度小于等于 100 。大于 100 的情况会进行截断。不能为空。
-5. regionID 和 timestamp 的长度需要完全相同，而且长度等于 length 。要求越靠近链表尾部时间越晚；也就是说按照时间的降序排序。
-6. timestamp 是一个以格式是 '%Y-%m-%d %H:%M:%S'的时间为元素的链表。不能为空。
+3. topK 表示提供几个备选方案。可以为空。默认值是1个。
+4. regionID 是一个以正整数（不包含0的自然数）为元素的链表。0 是作为填充值存在的。不能为空。
+5. length 表示的是 regionID 和 timestamp 所对应的链表的长度。默认情况下 length 的长度小于等于 100 。大于 100 的情况会进行截断。不能为空。
+6. regionID 和 timestamp 的长度需要完全相同，而且长度等于 length 。要求越靠近链表尾部时间越晚；也就是说按照时间的降序排序。
+7. timestamp 是一个以格式是 '%Y-%m-%d %H:%M:%S'的时间为元素的链表。不能为空。
 
 ### test data
 
@@ -31,6 +33,7 @@ uvicorn HttpService:app --host 0.0.0.0 --port 8000 --reload
 {
     "clientID": 100,
     "userID": 200,
+    "topK": 3,
     "length": 3,
     "regionID": [1002, 2345, 5678],
     "timestamp": ["2025-01-28 08:00:00", "2025-02-10 09:10:00", "2025-03-05 08:20:00"]
